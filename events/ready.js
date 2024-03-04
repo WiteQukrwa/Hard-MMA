b7874a6e9392c405a6607d6ce21cd08ef3ebda1f
WiteQukrwa <128302873+WiteQukrwa@users.noreply.github.com>
2024-03-04 16:58:24 +0100

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.5`, 
+  name: `Hard MMA #1 10.03.2024 18:00 `, 
   type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
 } else {
